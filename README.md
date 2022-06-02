684e17ec0a76753995fb349ee1aacd040576aef8
Axton Smith <axtonsmith@gmail.com>
2022-06-01 23:07:30 -0400

    Finish operator buttons

--- README.md
+++ README.md
@@ -2,20 +2,7 @@
 
 Basic calculator app, part of The Odin Project lessons.  
 
-## Finished:  
-
--Built add, subtract, multiply, and divide functions  
--Built an operator function that takes an operator and 2 numbers and then calls one of the operator functions on the numbers  
--Created the HTML skeleton for the calculator, minimal styling, labelled all the buttons, put a display element
--Built the button click functions to print to display  
-
-## Current:  
-
--Make the calculator work! You’ll need to store the first number that is input into the calculator when a user presses an operator, and also save which operation has been chosen and then operate() on them when the user presses the “=” key.  
-a) You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation.  
-b) This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.  
-
-## To do:  
+## Current stage:  
 
 -Gotchas: watch out for and fix these bugs if they show up in your code:  
 a) Users should be able to string together several operations and get the right answer, with each pair of numbers being evaluated at a time. For example, 12 + 7 - 5 * 3 = should yield 42. An example of the behavior we’re looking for would be this student solution.  
@@ -25,7 +12,19 @@ d) Pressing = before entering all of the numbers or an operator could cause prob
 e) Pressing “clear” should wipe out any existing data.. make sure the user is really starting fresh after pressing “clear”  
 f) Display a snarky error message if the user tries to divide by 0… don’t let it crash your calculator!  
 
-## Upcoming:  
+## Current features:  
+
+-Built add, subtract, multiply, and divide functions  
+-Built an operator function that takes an operator and 2 numbers and then calls one of the operator functions on the numbers  
+-Created the HTML skeleton for the calculator, minimal styling, labelled all the buttons, put a display element
+-Built the button click functions to print to display
+-Built functions into operator buttons  
+
+## Roadmap to launch:  
+
+-Finish testing  
+
+## Planned updates:  
 
 -Users can get floating point numbers if they do the math required to get one, but they can’t type them in yet. Add a . button and let users input decimals! Make sure you don’t let them type more than one though: 12.3.56.5. It is hard to do math on these numbers. (disable the decimal button if there’s already one in the display)  
 -Make it look nice! This is a great project to practice your CSS skills. At least make the operations a different color from the keypad buttons.  
--- script.js
+++ script.js
@@ -1,26 +1,35 @@
+let previousValue = 0;
+let displayValue = '0';
+let previousOperator = '';
+
 function add(a, b){
-    return a + b;
+    return (a + b).toString();
 }
 function subtract(a, b){
-    return a - b;
+    return (a - b).toString();
 }
 function multiply(a, b){
-    return a * b;
+    return (a * b).toString();
 }
 function divide(a, b){
-    return a / b;
+    return (a / b).toString();
 }
 function operate(operator, a, b){
     return operator(a, b);
 }
 
 const display = document.querySelector('#display');
-let displayValue = '0';
 display.textContent = displayValue;
 
+
+//number buttons
 const one = document.querySelector('#one');
 one.addEventListener('click', function(){
     if(displayValue === '0'){
+        displayValue = '1';
+        display.textContent = displayValue;
+    } else if(previousOperator === 'equals'){
+        previousOperator = '';
         displayValue = '1';
         display.textContent = displayValue;
     } else {
@@ -33,6 +42,10 @@ two.addEventListener('click', function(){
     if(displayValue === '0'){
         displayValue = '2';
         display.textContent = displayValue;
+    } else if(previousOperator === 'equals'){
+        previousOperator = '';
+        displayValue = '2';
+        display.textContent = displayValue;
     } else {
         displayValue += '2';
         display.textContent = displayValue;
@@ -41,6 +54,10 @@ two.addEventListener('click', function(){
 const three = document.querySelector('#three');
 three.addEventListener('click', function(){
     if(displayValue === '0'){
+        displayValue = '3';
+        display.textContent = displayValue;
+    } else if(previousOperator === 'equals'){
+        previousOperator = '';
         displayValue = '3';
         display.textContent = displayValue;
     } else {
@@ -53,6 +70,10 @@ four.addEventListener('click', function(){
     if(displayValue === '0'){
         displayValue = '4';
         display.textContent = displayValue;
+    } else if(previousOperator === 'equals'){
+        previousOperator = '';
+        displayValue = '4';
+        display.textContent = displayValue;
     } else {
         displayValue += '4';
         display.textContent = displayValue;
@@ -61,6 +82,10 @@ four.addEventListener('click', function(){
 const five = document.querySelector('#five');
 five.addEventListener('click', function(){
     if(displayValue === '0'){
+        displayValue = '5';
+        display.textContent = displayValue;
+    } else if(previousOperator === 'equals'){
+        previousOperator = '';
         displayValue = '5';
         display.textContent = displayValue;
     } else {
@@ -73,6 +98,10 @@ six.addEventListener('click', function(){
     if(displayValue === '0'){
         displayValue = '6';
         display.textContent = displayValue;
+    } else if(previousOperator === 'equals'){
+        previousOperator = '';
+        displayValue = '6';
+        display.textContent = displayValue;
     } else {
         displayValue += '6';
         display.textContent = displayValue;
@@ -81,6 +110,10 @@ six.addEventListener('click', function(){
 const seven = document.querySelector('#seven');
 seven.addEventListener('click', function(){
     if(displayValue === '0'){
+        displayValue = '7';
+        display.textContent = displayValue;
+    } else if(previousOperator === 'equals'){
+        previousOperator = '';
         displayValue = '7';
         display.textContent = displayValue;
     } else {
@@ -93,6 +126,10 @@ eight.addEventListener('click', function(){
     if(displayValue === '0'){
         displayValue = '8';
         display.textContent = displayValue;
+    } else if(previousOperator === 'equals'){
+        previousOperator = '';
+        displayValue = '8';
+        display.textContent = displayValue;
     } else {
         displayValue += '8';
         display.textContent = displayValue;
@@ -101,6 +138,10 @@ eight.addEventListener('click', function(){
 const nine = document.querySelector('#nine');
 nine.addEventListener('click', function(){
     if(displayValue === '0'){
+        displayValue = '9';
+        display.textContent = displayValue;
+    } else if(previousOperator === 'equals'){
+        previousOperator = '';
         displayValue = '9';
         display.textContent = displayValue;
     } else {
@@ -113,19 +154,99 @@ zero.addEventListener('click', function(){
     if(displayValue === '0'){
         displayValue = '0';
         display.textContent = displayValue;
+    } else if(previousOperator === 'equals'){
+        previousOperator = '';
+        displayValue = '0';
+        display.textContent = displayValue;
     } else {
         displayValue += '0';
         display.textContent = displayValue;
     } 
 });
+
+//operator buttons
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', function(){
+    previousOperator = '';
+    previousValue = '0';
     displayValue = '0';
     display.textContent = displayValue;
-})
+});
 
-//const button = document.querySelectorAll('button');
-//function printToDisplay(){
-//    this.
-//    display.textContent = '1';
-//};
+const addButton = document.querySelector('#add');
+addButton.addEventListener('click', function(){
+    if(previousOperator === ''){
+        previousValue = displayValue;
+    } else {
+    const a = parseFloat(previousValue);
+    const b = parseFloat(displayValue);
+    previousValue = operate(add, a, b);    
+    } previousOperator = 'add';
+    displayValue = '0';
+    display.textContent = displayValue;
+});
+const subtractButton = document.querySelector('#subtract');
+subtractButton.addEventListener('click', function(){
+    if(previousOperator === ''){
+        previousValue = displayValue;
+    } else {
+    const a = parseFloat(previousValue);
+    const b = parseFloat(displayValue);
+    previousValue = operate(subtract, a, b);    
+    } previousOperator = 'subtract';
+    displayValue = '0';
+    display.textContent = displayValue;});
+const multiplyButton = document.querySelector('#multiply');
+multiplyButton.addEventListener('click', function(){
+    if(previousOperator === ''){
+        previousValue = displayValue;
+    } else {
+    const a = parseFloat(previousValue);
+    const b = parseFloat(displayValue);
+    previousValue = operate(multiply, a, b);    
+    } previousOperator = 'multiply';
+    displayValue = '0';
+    display.textContent = displayValue;
+});
+const divideButton = document.querySelector('#divide');
+divideButton.addEventListener('click', function(){
+    if(previousOperator === ''){
+        previousValue = displayValue;
+    } else {
+    const a = parseFloat(previousValue);
+    const b = parseFloat(displayValue);
+    previousValue = operate(divide, a, b);    
+    } previousOperator = 'divide';
+    displayValue = '0';
+    display.textContent = displayValue;
+});
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', function(){
+    if(previousOperator === 'equals'){
+        display.textContent = displayValue;
+    } else if(previousOperator === 'add'){
+        const a = parseFloat(previousValue);
+        const b = parseFloat(displayValue);
+        previousValue = '0';
+        displayValue = operate(add, a, b);
+        display.textContent = displayValue;
+    } else if(previousOperator === 'subtract'){
+        const a = parseFloat(previousValue);
+        const b = parseFloat(displayValue);
+        previousValue = '0';
+        displayValue = operate(subtract, a, b);
+        display.textContent = displayValue;
+    } else if(previousOperator === 'multiply'){
+        const a = parseFloat(previousValue);
+        const b = parseFloat(displayValue);
+        previousValue = '0';
+        displayValue = operate(multiply, a, b);
+        display.textContent = displayValue;
+    } else if(previousOperator === 'divide'){
+        const a = parseFloat(previousValue);
+        const b = parseFloat(displayValue);
+        previousValue = '0';
+        displayValue = operate(divide, a, b);
+        display.textContent = displayValue;
+    } previousOperator = 'equals';
+});
